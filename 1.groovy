8f9d5348d9fd17302653e93228570557fdbbc735
user <user@Lenovo-PC.(none)>
2014-03-20 01:37:28 +0200

    changes 20/03/2014 2

--- 1.groovy
+++ 1.groovy
@@ -9,16 +9,13 @@ import groovy.sql.Sql
 
 class MyVisitor extends org.objectweb.asm.ClassVisitor {
     
-    String _hash
-    String _file
-    String _package
-    String _class
-    String _version
-    String _access
-    String _super
-    String _interfaces
-    
     def sql
+    def md5 = java.security.MessageDigest.getInstance("MD5")
+  
+    def _cls
+    def _file
+    
+    Integer i = 0;
     
 	MyVisitor() {
 	    super(org.objectweb.asm.Opcodes.ASM4, null)
@@ -37,45 +34,75 @@ class MyVisitor extends org.objectweb.asm.ClassVisitor {
                      PRIMARY KEY (id) );""" 
         sql.execute(query)
         
-        def query = """CREATE TABLE IF NOT EXISTS methods (
+        query = """CREATE TABLE IF NOT EXISTS methods (
                      id         varchar,
                      class      varchar, 
                      method     varchar,
+                     returns    varchar,
                      access     varchar,
                      exceptions varchar,
                      PRIMARY KEY (id) );""" 
+        sql.execute(query)
+        
+        query = """CREATE TABLE IF NOT EXISTS fields (
+                     id         varchar, 
+                     class      varchar, 
+                     field      varchar, 
+                     type       varchar, 
+                     access     varchar, 
+                     value      varchar,
+                     PRIMARY KEY (id) );"""
         sql.execute(query)
 	}
 	
 	
 	
 	void visit(int version, int access, String name, String signature, String superName, String[] interfaces){
-		_version = version
-		_access = access
-		_package = name.replace("/", ".")
-	    _class = name.replace("/", ".")    
-          try{ (_package, _class) = name.split(/[.](?=[^.]+$)/) }catch(Exception e){}
-		_super = superName.replace("/",".")
-		_interfaces = interfaces.collect({ it.replace("/",".") }).join(", ")
-		
-		sql.execute("INSERT INTO classes(id, version, access, file, package, class, super, interfaces) VALUES ( ${_hash}, ${_version}, ${_access}, ${_file}, ${_package}, ${_class}, ${_super}, ${_interfaces} );")
+        def _version = version
+        def _access = access
+        def _package = name.replace("/", ".")
+        def _class = name.replace("/", ".")    
+              try{ (_package, _class) = name.split(/[.](?=[^.]+$)/) }catch(Exception e){}
+        def _super = superName.replace("/",".")
+        def _interfaces = interfaces?.collect({ it.replace("/",".") }).join(", ")
+        
+        //md5.reset()
+        //def _hash = (new BigInteger(md5.digest(name.bytes))).toString(16)
+        def _hash = i++;
+        
+        _cls = _hash
+        
+        sql.execute("INSERT INTO classes(id, version, access, file, package, class, super, interfaces) VALUES ( ${_hash}, ${_version}, ${_access}, ${_file}, ${_package}, ${_class}, ${_super}, ${_interfaces} );")
 	}
 	
 	org.objectweb.asm.FieldVisitor visitField(int access, String name, String desc, String signature, Object value){
-		def _method = org.objectweb.asm.Type.getReturnType(desc).getClassName() + " " + name + " = " + value
-		
-		//println("Field: " + out)
-		sql.execute("INSERT INTO classes(id, class, method, access, exceptions) VALUES ( ${_hash}, ${_access}, ${_file}, ${_package}, ${_class}, ${_super}, ${_interfaces} );")
-		
-		return super.visitField(access, name, desc, signature, value)
+        def _method = org.objectweb.asm.Type.getReturnType(desc).getClassName() + " " + name + " = " + value
+        
+        def _field = name
+        def _type = desc
+        def _access = access
+        def _value = value.toString()
+    
+        //md5.reset()
+        //def _hash = (new BigInteger(md5.digest(name.bytes))).toString(16)
+        def _hash = i++;
+        
+        sql.execute("INSERT INTO fields(id, class, field, type, access, value) VALUES ( ${_hash}, ${_cls}, ${_field}, ${_type}, ${_access}, ${_value} );")
+        
+        return super.visitField(access, name, desc, signature, value)
 	}
 	
 	org.objectweb.asm.MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions){
-		//def _name = org.objectweb.asm.Type.getReturnType(desc).getClassName() + " " + 
-		def _name = name + "(" + org.objectweb.asm.Type.getArgumentTypes(desc).collect({ it.getClassName() }).join(", ") + ")"
-		
-        //println("Method: " + out)
-        sql.execute("INSERT INTO classes(id, class, method, access, exceptions) VALUES ( ${_hash}, ${_access}, ${_file}, ${_package}, ${_class}, ${_super}, ${_interfaces} );")
+		    def _method = name + "(" + org.objectweb.asm.Type.getArgumentTypes(desc).collect({ it.getClassName() }).join(", ") + ")"
+		    def _returns = org.objectweb.asm.Type.getReturnType(desc).getClassName()
+		    def _access = access
+		    def _exceptions = exceptions?.join(", ")
+
+		    //md5.reset()
+		    //def _hash = (new BigInteger(md5.digest((name + desc + signature).bytes))).toString(16)
+		    def _hash = i++;
+		    
+        sql.execute("INSERT INTO methods(id, class, method, returns, access, exceptions) VALUES ( ${_hash}, ${_cls}, ${_method}, ${_returns}, ${_access}, ${_exceptions} );")
         return super.visitMethod(access, name, desc, signature, exceptions)
     }
 }
@@ -94,6 +121,7 @@ dir.eachFileRecurse(FileType.FILES) { f ->
 		entries.each { entry->
 			if (entry.name.endsWith(".class")) {
 				//println("Class file: " + entry)
+				visitor._file = entry.toString()
 				def is = zip.getInputStream(entry)
 				cr = new org.objectweb.asm.ClassReader(is)
 				cr.accept(visitor, 0)
--- 5.groovy
+++ 5.groovy
@@ -1,4 +1,5 @@
 @Grapes([
+    @Grab(group='org.ow2.asm', module='asm', version='4.2'),
     @Grab(group='com.orientechnologies', module='orientdb-core', version='1.6.3'),
     @Grab(group='com.orientechnologies', module='orient-commons', version='1.6.3'),
     @Grab(group='com.orientechnologies', module='orientdb-tools', version='1.6.4')
@@ -7,7 +8,6 @@
 import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx
 import com.orientechnologies.orient.core.record.impl.ODocument
 
-import com.orientechnologies.orient.console.OConsoleDatabaseApp
 
 def ORIENTDB_URI = "local:./orientDB"
 def ORIENTDB_USER = "admin"
@@ -20,13 +20,93 @@ try {
     db.create()
 }
 
-ODocument cls = new ODocument("Class");
-cls.field( "name", "TestClass" );
-cls.field( "file", "Some" );
-cls.save();
+class MyVisitor extends org.objectweb.asm.ClassVisitor {
+
+    def _file
+    def _class
+    def _methods = []
+    def _fields = []
+  
+    MyVisitor() {
+	    super(org.objectweb.asm.Opcodes.ASM4, null)
+	  }
+	  
+	  void visit(int version, int access, String name, String signature, String superName, String[] interfaces){
+	      _class = new ODocument("Class")  
+	    
+	      _class.field("file", _file)
+	      _class.field("varsion", version)
+        _class.field("access", access)
+        _class.field("name", name.replace("/", "."))
+        try{ 
+            def (_pkg, _cls) = name.split(/[.](?=[^.]+$)/)
+            _class.field("package", _pkg)
+            _class.field("class", _cls)
+        } catch(Exception e){}
+        _class.field("super", superName.replace("/",".")) 
+        _class.field("interfaces", interfaces?.collect({ it.replace("/",".") }).join(", ")) 
+    }
+    
+    void visitEnd() {
+        _class.field("methods", _methods)
+        _class.field("fields", _fields)
+        _class.save()
+    }
+    
+    org.objectweb.asm.FieldVisitor visitField(int access, String name, String desc, String signature, Object value){
+        def _field = new ODocument("Field")
+        
+        _field.field("class", _class)
+        _field.field("name", name)
+        _field.field("type", org.objectweb.asm.Type.getReturnType(desc).getClassName() )
+        _field.field("access", access)
+        _field.field("value", value)
+        
+        _fields.push(_field)
+        
+        return super.visitField(access, name, desc, signature, value)
+    }
+
+    org.objectweb.asm.MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions){
+        def _method = new ODocument("Method")  
+        
+        _method.field("class", _class)
+        _method.field("name", name)
+        _method.field("method", name + "(" + org.objectweb.asm.Type.getArgumentTypes(desc).collect({ it.getClassName() }).join(", ") + ")")
+        _method.field("return", org.objectweb.asm.Type.getReturnType(desc).getClassName() )
+        _method.field("access", access)
+        _method.field("exceptions", exceptions?.join(", "))
+        
+        _methods.push(_method)
+        
+        return super.visitMethod(access, name, desc, signature, exceptions)
+    }
+    
+    def scanDirectory(File dir){
+        dir.eachFileRecurse(groovy.io.FileType.FILES) { f ->
+          if(f.name.endsWith('.jar')) {
+            //println("File: " + f)
+            def zip = new java.util.zip.ZipFile(f) 
+            zip.entries().each { entry->
+              if (entry.name.endsWith(".class")) {
+                //println("Class file: " + entry)
+                _file = entry.toString()
+                
+                def is = zip.getInputStream(entry)
+                def cr = new org.objectweb.asm.ClassReader(is)
+                cr.accept(this, 0)
+                is.close()
+        } } } }
+    }
+}
+
+def visitor = new MyVisitor()
+visitor.scanDirectory( new File("D:\\hybris\\bin\\platform\\bootstrap\\") )
 
 db.close()
 
+
+import com.orientechnologies.orient.console.OConsoleDatabaseApp
 
 def console = new OConsoleDatabaseApp()
 console.connect(ORIENTDB_URI, ORIENTDB_USER, ORIENTDB_PASS)
